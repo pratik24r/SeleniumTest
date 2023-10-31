83ffe0db950c30090e09df2efde101ce86983495
pratik rathi <pratik.rathi@fineshift.com>
2023-10-31 11:20:42 +0530

    demo project

---
+++ src/test/java/Testcases/Employee.java
@@ -0,0 +1,16 @@
+package Testcases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Employee {
+	
+	@Test
+	public void test1() {
+		System.out.println("first test case");
+	}
+	
+	
+	
+
+}
