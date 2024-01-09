d6ffaf8255c0969ce322220827cca44df990ad0d
pratik rathi <pratik.rathi@fineshift.com>
2024-01-09 11:51:09 +0530

    add to local repo

--- src/test/java/Testcases/Employee.java
+++ src/test/java/Testcases/Employee.java
@@ -7,15 +7,18 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Employee {
+WebDriver driver;	
 	@Test
 	public void Validate_url() {
 		
+		System.setProperty("webdriver.chrome.driver","//home//fs-pratik//Downloads//chromedriver-linux64//chromedriver.exe");
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.com/");
 		
 	}
+	
 	
 	
 	
