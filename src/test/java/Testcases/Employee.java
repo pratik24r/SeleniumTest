3b70263fea507ac81266c6bd6fd72dd6fed94a92
pratik rathi <pratik.rathi@fineshift.com>
2024-01-09 18:46:54 +0530

    browser updated

--- src/test/java/Testcases/Employee.java
+++ src/test/java/Testcases/Employee.java
@@ -2,23 +2,34 @@ package Testcases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Employee {
 WebDriver driver;	
-	@Test
+	@BeforeClass
 	public void Validate_url() {
 		
-		System.setProperty("webdriver.chrome.driver","//home//fs-pratik//Downloads//chromedriver-linux64//chromedriver.exe");
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+	//	System.setProperty("webdriver.edge.driver", "\\home\\fs-pratik\\Downloads\\edgedriver-linux64\\msedgedriver.exe");
+		WebDriverManager.edgedriver().setup(); ///home/fs-pratik/Downloads/edgedriver_linux64
+		WebDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.com/");
 		
 	}
 	
+	@Test
+	public void testCase1() {
+		System.out.println("this is testCase1");
+	}
+	
+	@Test
+	public void testCase2() {
+		System.out.println("this is testCase2");
+	}
 	
 	
 	
