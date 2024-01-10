21e3c5c3076e209c8f632fa92b6b1146289d427b
pratik rathi <pratik.rathi@fineshift.com>
2024-01-10 14:55:47 +0530

    updated file

--- src/test/java/Testcases/Employee.java
+++ src/test/java/Testcases/Employee.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -16,12 +17,15 @@ WebDriver driver;
 		
 		System.setProperty("webdriver.edge.driver", "/home/fs-pratik/Downloads/edgedriver_linux64/msedgedriver");
 	//	WebDriverManager.edgedriver().setup(); ///home/fs-pratik/Downloads/edgedriver_linux64
-		WebDriver driver = new EdgeDriver();
 		EdgeOptions options = new EdgeOptions();
-		options.addArguments("--disable-logging");
-		
-		driver.manage().window().maximize();
-		driver.get("https://www.amazon.com/");
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("--no-sandbox"); // Bypass OS security model
+		WebDriver driver = new EdgeDriver(options);
+		driver.get("https://google.com");
 		
 		
 		
@@ -37,10 +41,16 @@ WebDriver driver;
 	public void testCase2() {
 		System.out.println("this is testCase2");
 	}
+	@AfterMethod
+    public void tearDown() {
+        // Close the browser after each test method
+        if (driver != null) {
+            driver.quit();
+        }
 	
 	
 	
 	
-	
+	}	
 
 }
