e2a9b4f3c155b4f3b53ec1867e045418645dd326
pratik rathi <pratik.rathi@fineshift.com>
2024-01-11 14:11:58 +0530

    permission updated

--- src/test/java/Testcases/Employee.java
+++ src/test/java/Testcases/Employee.java
@@ -3,8 +3,6 @@ package Testcases;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -15,17 +13,12 @@ WebDriver driver;
 	@BeforeClass
 	public void Validate_url() {
 		
-		System.setProperty("webdriver.edge.driver", "/home/fs-pratik/Downloads/edgedriver_linux64/msedgedriver");
-	//	WebDriverManager.edgedriver().setup(); ///home/fs-pratik/Downloads/edgedriver_linux64
-		EdgeOptions options = new EdgeOptions();
-		options.addArguments("start-maximized"); // open Browser in maximized mode
-		options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("--no-sandbox"); // Bypass OS security model
-		WebDriver driver = new EdgeDriver(options);
-		driver.get("https://google.com");
+	//	System.setProperty("webdriver.edge.driver", "/home/fs-pratik/Downloads/edgedriver_linux64/msedgedriver");
+		WebDriverManager.edgedriver().setup(); ///home/fs-pratik/Downloads/edgedriver_linux64
+		WebDriver driver = new EdgeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.com/");
 		
 		
 		
@@ -41,16 +34,6 @@ WebDriver driver;
 	public void testCase2() {
 		System.out.println("this is testCase2");
 	}
-	@AfterMethod
-    public void tearDown() {
-        // Close the browser after each test method
-        if (driver != null) {
-            driver.quit();
-        }
 	
-	
-	
-	
-	}	
 
 }
